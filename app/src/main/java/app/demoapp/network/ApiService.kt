2a16a06f4c91d407a7ec0948eae122d8a381491a
Minh Hiển Phan <phanminhhien0701@gmail.com>
2025-03-27 23:23:10 +0700

    [INITIAL] setup project and add dependency

---
+++ app/src/main/java/app/demoapp/network/ApiService.kt
@@ -0,0 +1,3 @@
+package app.demoapp.network
+
+//https://github.com/google-developer-training/basic-android-kotlin-compose-training-mars-photos/blob/repo-starter/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
---
+++ app/src/main/java/app/demoapp/ui/screens/HomeScreen.kt
@@ -0,0 +1,8 @@
+package app.demoapp.ui.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen()  {
+
+}
